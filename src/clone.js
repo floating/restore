77a5e5258c2ec468b6d438796dea79f27603dc8f
C. Jordan Muir <cjm712@gmail.com>
2017-07-04 13:41:25 -0400

    Update clone method

--- src/clone.js
+++ src/clone.js
@@ -2,5 +2,17 @@
   Clone JSON objects
 */
 
-export const clone = (obj) => JSON.parse(JSON.stringify(obj))
+export const clone = o => {
+  let n, i
+  if (typeof o !== 'object') return o
+  if (!o) return o
+  if (o.constructor === Array) {
+    n = []
+    for (i = 0; i < o.length; i++) n[i] = clone(o[i])
+    return n
+  }
+  n = {}
+  for (i in o) n[i] = clone(o[i])
+  return n
+}
 export default clone
