db144814f3dafd84dbe4f1a1023f47f7e29e854c
C. Jordan Muir <cjm712@gmail.com>
2017-08-05 17:07:30 -0400

    Simplify loadStyle

--- src/DevTools/shared/loadStyle.js
+++ src/DevTools/shared/loadStyle.js
@@ -1,16 +1,15 @@
-let isNode = (typeof process === 'object' && typeof process.versions === 'object' && typeof process.versions.node !== 'undefined')
-
-let style = css => {
-  if (isNode) return
-  let head = document.head || document.getElementsByTagName('head')[0]
-  let style = document.createElement('style')
-  style.type = 'text/css'
-  if (style.styleSheet) {
-    style.styleSheet.cssText = css
-  } else {
-    style.appendChild(document.createTextNode(css))
+export const loadStyle = css => {
+  if (typeof window !== 'undefined' && window.document && window.document.createElement) {
+    let head = document.head || document.getElementsByTagName('head')[0]
+    let style = document.createElement('style')
+    style.type = 'text/css'
+    if (style.styleSheet) {
+      style.styleSheet.cssText = css
+    } else {
+      style.appendChild(document.createTextNode(css))
+    }
+    head.appendChild(style)
   }
-  head.appendChild(style)
 }
 
-export default style
+export default loadStyle
